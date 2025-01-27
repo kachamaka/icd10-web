390d5b5483b599ed9014775099b6087d9c658fef
unknown <martin.popov42@gmail.com>
2025-01-27 20:26:57 +0200

    Change ec2 ip

--- src/app/services/icd10-search.service.ts
+++ src/app/services/icd10-search.service.ts
@@ -8,7 +8,7 @@ import { Icd10SearchResult } from '../models/icd10-search-result';
   providedIn: 'root'
 })
 export class Icd10SearchService {
-  private apiBase = 'http://16.171.249.214:8080'; // Replace with your actual API base URL
+  private apiBase = 'http://16.171.177.148:8080'; // Replace with your actual API base URL
   // private apiBase = 'https://5xsmx2omy8.execute-api.eu-north-1.amazonaws.com'; 
 
   constructor(private http: HttpClient) {}
